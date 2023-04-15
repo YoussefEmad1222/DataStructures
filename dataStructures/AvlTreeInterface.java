081bdd87a929e5626cf9079fd55663d6b2635700
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-04-16 00:06:59 +0200

    change

---
+++ dataStructures/AvlTreeInterface.java
@@ -0,0 +1,23 @@
+package dataStructures;
+interface AvlTree {
+    public int size();
+
+    public boolean isEmpty();
+
+    public boolean search(int data);
+
+    public int FindMin();
+
+    public int FindMax();
+
+    public void insert(int data);
+
+    public void delete(int data);
+
+    public void inorder();
+
+    public void preorder();
+
+    public void postorder();
+
+}
