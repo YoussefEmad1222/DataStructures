598c494e75af06d9eb31c66b2e7f969cde0a61d2
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-04-16 04:22:03 +0200

    Update bellmanFord.java

--- Algorithms/bellmanFord.java
+++ Algorithms/bellmanFord.java
@@ -1,4 +1,25 @@
 package Algorithms;
+class edge {
+    int source;
+    int destination;
+    double value;
+
+    edge(int source, int destination, double value) {
+        this.source = source;
+        this.destination = destination;
+        this.value = value;
+    }
+}
+
+class node {
+    int index;
+    double value;
+
+    node(int index, double value) {
+        this.index = index;
+        this.value = value;
+    }
+}
 
 public class bellmanFord {
     
