28e37c8e5d30a9619415a70f6bfd41d82feacb86
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2023-05-17 15:13:13 +0300

    Update prim.java

--- Algorithms/prim.java
+++ Algorithms/prim.java
@@ -3,43 +3,41 @@ import java.util.Set;
 
 public class prim {
     int prims(int[][] graph) {
-        int V = graph.length;
-        int[] values = new int[V];
-        int[] parents = new int[V];
-        boolean[] includedInMST = new boolean[V];
+        int numVertices = graph.length;
+        int[] vertexValues = new int[numVertices];
+        int[] parentVertices = new int[numVertices];
+        boolean[] includedInMST = new boolean[numVertices];
 
-        // Initialize all values to infinity and parents to -1
-        for (int i = 0; i < V; i++) {
-            parents[i] = -1;
+        // Initialize all vertexValues to infinity and parentVertices to -1
+        for (int i = 0; i < numVertices; i++) {
+            parentVertices[i] = -1;
             includedInMST[i] = false;
-            values[i] = Integer.MAX_VALUE;
+            vertexValues[i] = Integer.MAX_VALUE;
         }
 
         // Set value of first vertex to 0 and start from there
-        values[0] = 0;
+        vertexValues[0] = 0;
 
-        // Loop over all vertices except the last one
-        for (int i = 0; i < V - 1; i++) {
-            int minIndex = getMinimumEdgeIndex(includedInMST, values);
-            includedInMST[minIndex] = true;
+        // Loop over all vertices except the last one to find the minimum spanning tree
+        for (int i = 0; i < numVertices - 1; i++) {
+            // Find the vertex with the minimum value
+            int minIndex = getMinimumEdgeIndex(includedInMST, vertexValues);
+            includedInMST[minIndex] = true;// mark the vertex as included in the MST
             // Update the values of all adjacent vertices
-            for (int j = 0; j < V; j++) {
-                if (graph[minIndex][j] != 0 && !includedInMST[j] && graph[minIndex][j] < values[j]) {
-                    parents[j] = minIndex;
-                    values[j] = graph[minIndex][j];
+            for (int j = 0; j < numVertices; j++) {
+                if (graph[minIndex][j] != 0 && !includedInMST[j] && graph[minIndex][j] < vertexValues[j]) {
+                    parentVertices[j] = minIndex;
+                    vertexValues[j] = graph[minIndex][j];
                 }
             }
         }
-        // printArray(parents);
-        for (int i = 0; i < V; i++) {
-            System.out.println("parents[" + i + "] => " + parents[i] + " values[" + i + "] => " + values[i]);
+
+        // get the total weight of the spanning tree
+        int totalWeight = 0;
+        for (int i = 0; i < numVertices; i++) {
+            totalWeight += vertexValues[i];
         }
-        // get the value of the spanning tree
-        int sum = 0;
-        for (int i = 0; i < V; i++) {
-            sum += values[i];
-        }
-        return sum;
+        return totalWeight;
     }
 
     // Helper function to get the index of the vertex with the minimum value
@@ -55,6 +53,7 @@ public class prim {
         return minIndex;
     }
 
+    // Function to find the minimum cost to connect all the points in a 2d plane
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         int[][] graph = new int[n][n];
@@ -68,24 +67,28 @@ public class prim {
     }
 
     public int minCostOfEdgeList(int[][] edges) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> nodes = new HashSet<>();
         for (int i = 0; i < edges.length; i++) {
             int source = edges[i][0];
             int destination = edges[i][1];
-            set.add(source);
-            set.add(destination);
+            nodes.add(source);
+            nodes.add(destination);
         }
-        int[][] graph = new int[set.size()][set.size()];
+
+        int[][] graph = new int[nodes.size()][nodes.size()];
         for (int i = 0; i < edges.length; i++) {
             int source = edges[i][0];
             int destination = edges[i][1];
-            graph[source][destination] = edges[i][2];
+            int cost = edges[i][2];
+            graph[source][destination] = cost;
+            graph[destination][source] = cost;
         }
+
         return prims(graph);
     }
 
     public static void main(String[] args) {
-        Solution s = new Solution();
+        prim s = new prim();
         int[][] edges = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
         System.out.println(s.minCostOfEdgeList(edges));
         int[][] points = { { 0, 0 }, { 2, 2 }, { 3, 10 }, { 5, 2 }, { 7, 0 } };
